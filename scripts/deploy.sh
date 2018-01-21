deac4aae8965ec8ea7dd00003e3ce7354c11c0ce
Gary Meehan <garymeehan@outlook.com>
2018-01-21 21:36:04 +0000

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,11 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 id_rsa # Allow read access to the private key
 ssh-add id_rsa # Add the private key to SSH
 
-git config --global push.default matching
-git remote add deploy ssh://deploy@$IP:$DEPLOY_DIR
-git push deploy master
+
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh deploy@$IP <<EOF
-  cd $DEPLOY_DIR
+  cd $DEPLOY_DIR &&
+  git pull &&
   yarn install && yarn build && yarn restart
 EOF
