72a02f93ab2d05ace9f03648df644cd9df0e9c9f
Gary Meehan <garymeehan@outlook.com>
2018-01-21 21:09:04 +0000

    add back deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo "Script"
-# echo starting deploy
-# eval "$(ssh-agent -s)" # Start ssh-agent cache
-# chmod 600 id_rsa # Allow read access to the private key
-# ssh-add id_rsa # Add the private key to SSH
 
-# git config --global push.default matching
-# git remote add deploy ssh://deploy@$IP:$DEPLOY_DIR
-# git push deploy master
+echo "running deploy script"
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 id_rsa # Allow read access to the private key
+ssh-add id_rsa # Add the private key to SSH
 
-# # Skip this command if you don't need to execute any additional commands after deploying.
-# ssh deploy@$IP <<EOF
-#   cd $DEPLOY_DIR
-#   yarn install && yarn build && yarn restart
-# EOF
+git config --global push.default matching
+git remote add deploy ssh://deploy@$IP:$DEPLOY_DIR
+git push deploy master
+
+# Skip this command if you don't need to execute any additional commands after deploying.
+ssh deploy@$IP <<EOF
+  cd $DEPLOY_DIR
+  yarn install && yarn build && yarn restart
+EOF
