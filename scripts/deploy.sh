c9a3e29aca1561a271793cced74bfff65555bc67
Gary Meehan <garymeehan@outlook.com>
2018-01-21 21:40:08 +0000

    disable pseudo-tty allocation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,12 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 id_rsa # Allow read access to the private key
 ssh-add id_rsa # Add the private key to SSH
 
-
+git config --global push.default matching
+git remote add deploy ssh://deploy@$IP:$DEPLOY_DIR
+git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.
-ssh deploy@$IP <<EOF
-  cd $DEPLOY_DIR &&
-  git pull &&
+ssh -T deploy@$IP <<EOF
+  cd $DEPLOY_DIR
   yarn install && yarn build && yarn restart
 EOF
