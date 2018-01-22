372ea9d085f037fbcc28e6007d791772e613ddc0
Gary Meehan <garymeehan@outlook.com>
2018-01-22 20:42:50 +0000

    remove if

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -x
-if [ $TRAVIS_BRANCH == 'master' ] ; then
-    # Initialize a new git repo in _site, and push it to our server.
-    cd build
-    git init
 
-    git remote add deploy "app_deploy@178.62.2.65:/var/www/crypto"
-    git config user.name "Travis CI"
-    git config user.email "garymeehan+travisCI@outlook.com"
+# Initialize a new git repo in _site, and push it to our server.
+cd build
+git init
 
-    git add .
-    git commit -m "Deploy"
-    git push --force deploy master
-else
-    echo "Not deploying, since this branch isn't master."
-fi
+git remote add deploy "app_deploy@178.62.2.65:/var/www/crypto"
+git config user.name "Travis CI"
+git config user.email "garymeehan+travisCI@outlook.com"
+
+git add .
+git commit -m "Deploy"
+git push --force deploy master
