560f341fa99c91959a4d854a5ebc0510bf66bed4
Gary Meehan <garymeehan@outlook.com>
2018-01-22 09:04:05 +0000

    fix typo

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,9 @@
-echo "install sh"
+#!/bin/bash
+set -x # Show the output of the following commands (useful for debugging)
+
+# Import the SSH deployment key
+openssl aes-256-cbc -K $encrypted_24baf089f7ed_key -iv $encrypted_24baf089f7ed_iv
+  -in id_rsa.enc -out id_rsa -d
+rm id_rsa.enc # Don't need it anymore
+chmod 600 id_rsa
+mv id_rsa ~/.ssh/id_rsa
