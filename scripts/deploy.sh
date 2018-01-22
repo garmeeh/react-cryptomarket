08d6c16bb362a8040420ea5e278e740f851055b5
Gary Meehan <garymeehan@outlook.com>
2018-01-22 15:36:55 +0000

    add port

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 
 git config --global push.default matching
-git remote add deploy ssh://deploy@$IP:$DEPLOY_DIR
+git remote add deploy ssh://deploy@$IP:22:$DEPLOY_DIR
 git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.
